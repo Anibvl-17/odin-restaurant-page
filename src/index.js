672970ca84d045aa548777b7dba66aac8cb73a43
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-23 23:23:04 -0300

    Add click listeners, add content update functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import "./styles.css";
+import './styles.css';
+import HomeDiv from './components/home.js';
+import MenuDiv from './components/menu.js';
+import AboutDiv from './components/about.js';
+
+const navButtons = document.querySelectorAll('.nav-btn');
+navButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    navButtons.forEach((button) => button.classList.remove('active'));
+    btn.classList.add('active');
+    switch (btn.id) {
+      case 'home-btn':
+        updateContent(HomeDiv());
+        break;
+      case 'menu-btn':
+        updateContent(MenuDiv());
+        break;
+      case 'about-btn':
+        updateContent(AboutDiv());
+        break;
+      default:
+        break;
+    }
+  });
+});
+
+function updateContent(component) {
+  const contentDiv = document.getElementById('content');
+  contentDiv.textContent = '';
+  contentDiv.appendChild(component);
+}
